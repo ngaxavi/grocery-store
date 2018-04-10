030b88dd3767f6b7a6a2b5df44105c8382930173
Xavier Ngansop <ngaxavi@gmail.com>
2018-04-10 12:09:49 +0200

    feat: integrate firebase in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
-
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,5 @@ export const environment = {
     projectId: 'grocery-store-fb4a8',
     storageBucket: '',
     messagingSenderId: '750459237491'
-
   }
 };
